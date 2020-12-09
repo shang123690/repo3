3b5f21168d07e462620f934eb1403e7665e28b62
shangting <13484519164@163.com>
2020-12-09 17:46:00 +0800

    17：45提交git-demo

---
+++ JavaEEHigh/git-demo/src/com/st/Test.java
@@ -0,0 +1,11 @@
+package com.st;
+
+/**
+ * @author shang
+ * @create 2020-12-09 17:41
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello Git,我来了");
+    }
+}
