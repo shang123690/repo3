6a094131785d58bfdf621c6d539b0a1db48500b4
shangting <13484519164@163.com>
2020-12-09 17:50:15 +0800

    17：49修改打印

--- JavaEEHigh/git-demo/src/com/st/Test.java
+++ JavaEEHigh/git-demo/src/com/st/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello Git,我来了");
         System.out.println("hello GitHub,我来了");
+        System.out.println("11111111111111111");
     }
 }
