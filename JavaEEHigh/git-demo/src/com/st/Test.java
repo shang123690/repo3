bc871cdab4f9b21af00ea3c3849576086457810f
shang123690 <73636877+shang123690@users.noreply.github.com>
2020-12-09 17:48:24 +0800

    Update Test.java

--- JavaEEHigh/git-demo/src/com/st/Test.java
+++ JavaEEHigh/git-demo/src/com/st/Test.java
@@ -7,5 +7,6 @@ package com.st;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello Git,我来了");
+        System.out.println("hello GitHub,我来了");
     }
 }
